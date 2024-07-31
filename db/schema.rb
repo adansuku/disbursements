21bb1c837eacb791420555f7393279eaff0a7010
Adán González <adangrx@gmail.com>
2024-07-31 11:07:53 +0100

    update schema after run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_30_192226) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_31_074252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "disbursements", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "reference", null: false
+    t.date "disbursed_at", null: false
+    t.uuid "merchant_id", null: false
+    t.decimal "amount_disbursed", precision: 10, scale: 2
+    t.decimal "amount_fees", precision: 10, scale: 2
+    t.decimal "total_order_amount", precision: 10, scale: 2
+    t.string "disbursement_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reference"], name: "index_disbursements_on_reference", unique: true
+  end
 
   create_table "merchants", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "reference"
@@ -29,8 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_30_192226) do
     t.decimal "amount", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "disbursement_id"
+    t.index ["disbursement_id"], name: "index_orders_on_disbursement_id"
     t.index ["merchant_id"], name: "index_orders_on_merchant_id"
   end
 
+  add_foreign_key "disbursements", "merchants"
+  add_foreign_key "orders", "disbursements"
   add_foreign_key "orders", "merchants"
 end
