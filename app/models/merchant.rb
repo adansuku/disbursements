c890c6a3a15f2f38bd225e1a120af9b906809568
Adán González <adangrx@gmail.com>
2024-07-31 08:19:46 +0100

    feat: added relation between merchants and orders

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,7 @@
-# app/models/merchant.rb
+# frozen_string_literal: true
 
+# Merchant model represents a merchant in the system.
+# A merchant can have many orders, which are dependent on the merchant.
 class Merchant < ApplicationRecord
+  has_many :orders, dependent: :destroy
 end
