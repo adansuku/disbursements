23134d8f1c25319bf77246cd382313bd872b0eab
Adán González <adangrx@gmail.com>
2024-08-01 10:31:24 +0100

    feat: add daily and weekly disbursement service

---
+++ spec/services/daily_disbursement_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe DailyDisbursementService do
+  describe '#perform' do
+    let(:merchant) { create(:merchant) }
+
+    it 'calculates and creates disbursements for merchants with daily frequency' do
+    end
+  end
+end
