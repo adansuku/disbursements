51ef68d79f558dcba684cdb90318c55f01b3cda7
Adán González <adangrx@gmail.com>
2024-08-01 10:25:09 +0100

    feat: add relation merchant - monthly_fees

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,6 +5,7 @@
 class Merchant < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :disbursements, through: :orders
+  has_many :monthly_fees, dependent: :destroy
 
   validates :email, presence: true
   validates :disbursement_frequency, presence: true
